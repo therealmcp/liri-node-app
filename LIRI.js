66748b69b3d6b9a87dcf34f3e67965967d394a62
unknown <cp.mcpike@gmail.com>
2018-10-12 17:41:30 -0700

    functionality working for concert-this and movie-this

--- LIRI.js
+++ LIRI.js
@@ -2,29 +2,12 @@ require("dotenv").config();
 
 var request = require('request');
 var moment = require('moment');
-var Spotify = require('node-spotify-api');
- 
-var spotify = new Spotify({
-  id: <your spotify client id>,
-  secret: <your spotify client secret>
-});
- 
-spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
-  }
- 
-console.log(data); 
-});
 
 var command = process.argv[2];
 var query = process.argv[3];
 
 switch (command) {
     case "movie-this":
-        if (query = null) {
-            query = "Mr.Nobody"
-        }
         request('http://www.omdbapi.com/?t=' + query + '&apikey=trilogy', function (error, response, body) {
             if (error) {
                 console.log('error:', error); // Print the error if one occurred
@@ -60,6 +43,6 @@ switch (command) {
             }
         });
         break;
-    case "spotify-this-song":
+    // case "spotify-this-song":
         
 }
